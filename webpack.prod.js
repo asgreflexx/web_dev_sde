a12bad00b175a137e8fcacca4e66f83790eb5b07
Michael Svoboda <michael.svoboda1@stud.fh-campuswien.ac.at>
2021-09-09 18:58:04 +0200

    added ci cd modified webpack prod conf

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,13 +8,55 @@ module.exports = {
     // Use the src/index.scripts file as entry point to bundle it.
     // If the src/index.scripts file imports other JS files,
     // bundle them as well
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: [
+      path.resolve(__dirname, './src/index.js'),
+      path.resolve(__dirname, './src/scripts/about.js'),
+      path.resolve(__dirname, './src/scripts/doggos.js'),
+      path.resolve(__dirname, './src/scripts/home.js'),
+    ],
+    module: {
+      // configuration regarding modules
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/, // files to exclude
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        // CSS and SASS
+        {
+          test: /\.(scss|css)$/,  // load files that end with scss and css
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(jpg|png)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',
+                publicPath: 'img/',
+              }
+            }
+          ]
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+          exclude: path.resolve(__dirname, 'src/index.html'),
+        },
+      ],
+    },
     // 2
     // The bundles source code files shall result in a bundle.scripts file
     // in the /dist folder
     output: {
       path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
     },
     // 3
     // The /dist folder will be used to serve our application
